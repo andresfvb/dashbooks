26259ef5b5e70c9873a5aec63c3295650e22def8
Andres Vasquez <barandicandres@gmail.com>
2023-09-29 01:19:48 -0500

    Cambiamos supabase 2

--- src/pages/api/auth/[...auth].js
+++ src/pages/api/auth/[...auth].js
@@ -1,8 +1,8 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-);
+const supabaseUrl = 'https://bzqfvoyafgzuisxfisrt.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ6cWZ2b3lhZmd6dWlzeGZpc3J0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU3ODE4NzAsImV4cCI6MjAxMTM1Nzg3MH0.Ohn7J6E0xyReUlslZYc8CyX8umN8JZtkNQjMrnqckh8';
 
-export { supabase };
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
