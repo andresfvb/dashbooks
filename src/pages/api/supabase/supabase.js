8a8e823159fb5d3165a8ee86e02c4e5b95dc5b47
Andres Vasquez <barandicandres@gmail.com>
2023-09-29 01:16:13 -0500

    Cambiamos supabase

--- src/pages/api/supabase/supabase.js
+++ src/pages/api/supabase/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://kgbnjkjewnxuerruqbrg.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtnYm5qa2pld254dWVycnVxYnJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU0OTIwNjksImV4cCI6MjAxMTA2ODA2OX0.P8D9wQv6CZc6ESQ_RpyuLr9GVrwD_Al4kXYepu51Kko';
+const supabaseUrl = 'https://bzqfvoyafgzuisxfisrt.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ6cWZ2b3lhZmd6dWlzeGZpc3J0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU3ODE4NzAsImV4cCI6MjAxMTM1Nzg3MH0.Ohn7J6E0xyReUlslZYc8CyX8umN8JZtkNQjMrnqckh8';
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
